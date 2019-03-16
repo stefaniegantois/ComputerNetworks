cfe967a5714e6d14d4814c2f59b1e505caaa4040
stefaniegantois <36856131+stefaniegantois@users.noreply.github.com>
2019-03-16 22:24:11 +0100

    statuscodes

--- Computer Networks/src/HTTPServer/HTTPResponse.java
+++ Computer Networks/src/HTTPServer/HTTPResponse.java
@@ -3,7 +3,31 @@ package HTTPServer;
 import HTTPClient.HTTPRequest;
 
 public  class HTTPResponse {
+	
+	private int statusCode;
+	
 	public HTTPResponse(HTTPRequest request) {
 		
+	}
+	
+	public String getStatus () {
+		switch(getStatusCode()) {
+		case 200:
+			return "OK";
+		case 304:
+			return "Not Modified";
+		case 400:
+			return "Bad Request";
+		case 404:
+			return "Not Found";
+		case 500:
+			return "Server Error";
+		default: 
+			return "Unknow Status Code";
+		}
+	}
+
+	private int getStatusCode() {
+		return statusCode;
 	}
 }
