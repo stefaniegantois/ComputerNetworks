f582dc4faa62e3add2e9b789d06b608a9c86a58d
GitteVO <33459446+GitteVO@users.noreply.github.com>
2019-03-19 22:48:49 +0100

    Update Server

--- Computer Networks/src/HTTPServer/Server.java
+++ Computer Networks/src/HTTPServer/Server.java
@@ -1,9 +1,26 @@
 package HTTPServer;
 
+import java.io.*;
+import java.net.*;
+
 public class Server {
-	
-	public Server () {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		ServerSocket welcomeSocket = new ServerSocket(80);
+		System.out.println("Server listening on port 80");
+		int conn = 0;
+		while(true) {
+			Socket client = welcomeSocket.accept();
+			if(client != null) {
+				conn += 1;
+				System.out.println("Made Connection." + conn);
+				Handler server = new Handler(client);
+				Thread thread = new Thread(server);
+				thread.start();
+			}
+		}
 	}
-	
-	
+
 }
+
